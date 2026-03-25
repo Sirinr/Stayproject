a85f03cbe7ab2ccc22bfa6bc6a2853f09fb8ad58
Sirin <sirinr95@gmail.com>
2026-03-25 19:30:03 +0100

    work in progress: myBookings layout + styling

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
 import './styles/base.css'
 import './styles/components.css'
 import './styles/pages.css'
+import { render } from './pages/myBookings/myBookings'
 
 import { Header } from './components/header'
 import { Footer } from './components/footer'
 import { Hero } from './components/hero'
-import { RoomsPage } from './pages/rooms/rooms'
+import { MyBookingsPage } from './pages/myBookings/myBookings'
 
 const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
-  app.innerHTML = `
-    ${Header()}
-
-    <main>
-      ${Hero()}
-      ${RoomsPage()}
-    </main>
-    
-    ${Footer()}
-  `
+  
+  render ()
 }
